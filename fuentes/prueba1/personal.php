9a4a4574dd4b149c6972c6aa627ee8e8217b515a
emilioplatzer@gmail.com <emilioplatzer@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-09-02 15:25:27 +0000

    Probamos una manera de enviar HTML a través de funciones, de modo que quede explícito el envío de contenido de texto y el envío de valores de atributos (que quedan del lado derecho de un array asociativo).

--- fuentes/prueba1/personal.php
+++ fuentes/prueba1/personal.php
@@ -1,5 +1,6 @@
 <?php
 require_once("comunes.php");
+require_once("armador-html.php");
 //require_once("main.html");
 function armaFormulario($titulo, $array_campos){
     echo<<<HTML
@@ -7,18 +8,28 @@ function armaFormulario($titulo, $array_campos){
     <table class=tabla_formulario> 
     <h1 class=titulo_formulario>$titulo</h1>
 HTML;
-    $str_html='';
     foreach($array_campos as $campo=>$definicion_campo){
         $leyenda=$definicion_campo['leyenda'];
         $aclaracion=$definicion_campo['aclaracion'];
+        enviar_abrir('tr');
+          enviar_abrir('td',array('class'=>'etiqueta_formulario', 'title'=>$aclaracion));
+            enviar_texto($leyenda);
+          enviar_cerrar('td');
+          enviar_abrir('td');
+            enviar_elemento('input',array('id'=>$campo, 'name'=>$campo, 'type'=>'text'));
+          enviar_cerrar('td');
+        enviar_cerrar('tr');
+        
+        /*
         seguro($leyenda,'html');
         seguro($aclaracion,'attr_html');
         $str_html=$str_html."
-            <tr><td class=etiqueta_formulario title='$aclaracion'>$leyenda</td>     
-            <td><input id=$campo name=$campo type='text'></td> 
-            </tr><tr><tr> ";
+            <tr>
+              <td class=etiqueta_formulario title='$aclaracion'>$leyenda</td>     
+              <td><input id=$campo name=$campo type='text'></td> 
+            </tr>";
+        */
     }
-    echo $str_html;
     echo <<<HTML2
     <td><div align="right"> 
     <input id="Restablecer" name="Restablecer" type="reset" id="Restablecer" value="Restablecer"> 
