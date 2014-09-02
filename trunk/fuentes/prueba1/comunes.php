8792c41511637d7f32d7f1094ccc1cfd61df15c9
emilioplatzer@gmail.com <emilioplatzer@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-09-02 15:06:29 +0000

    Cambio tabs por espacios.

--- trunk/fuentes/prueba1/comunes.php
+++ trunk/fuentes/prueba1/comunes.php
@@ -2,23 +2,22 @@
 
 date_default_timezone_set('America/Buenos_Aires');
 
-
 function seguro($algo, $uso='codigo'){
-	$conjuntos_seguros=array(
-		'codigo'=>'^[A-Za-z0-9_]*$',
-		'html'=>'^[^<>&]*$',
-		'attr_html'=>'^[^<>&"\']*$',
-		'mail'=>'^[A-Za-z0-9_@.]*$',
-		'dato_sql'=>"^[^']*$",
-	);
-	if(!isset($conjuntos_seguros[$uso])){
-		throw new Exception('no existe el uso para seguro');
-	}
-	$str_regexp=$conjuntos_seguros[$uso];
-	if(!preg_match('/'.$str_regexp.'/',$algo)){
-		throw new Exception("caracteres invalidos");
-	}
-	return $algo;
+    $conjuntos_seguros=array(
+        'codigo'=>'^[A-Za-z0-9_]*$',
+        'html'=>'^[^<>&]*$',
+        'attr_html'=>'^[^<>&"\']*$',
+        'mail'=>'^[A-Za-z0-9_@.]*$',
+        'dato_sql'=>"^[^']*$",
+    );
+    if(!isset($conjuntos_seguros[$uso])){
+        throw new Exception('no existe el uso para seguro');
+    }
+    $str_regexp=$conjuntos_seguros[$uso];
+    if(!preg_match('/'.$str_regexp.'/',$algo)){
+        throw new Exception("caracteres invalidos");
+    }
+    return $algo;
 }
 
 ?>
--- trunk/fuentes/prueba1/instalador.php
+++ trunk/fuentes/prueba1/instalador.php
@@ -5,7 +5,7 @@ require_once "db.class.php";
 
 $sql_campos=array();
 foreach($estructura_personal as $campo=>$definicion_campo){
-	$sql_campos[]="  $campo text";
+    $sql_campos[]="  $campo text";
 }
 
 $sql="CREATE TABLE prin.personal (\n".implode(",\n",$sql_campos)."\n);\n";
