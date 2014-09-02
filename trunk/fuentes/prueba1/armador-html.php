31e55554bf0cd5e4455d8f869dd252933aa464ec
emilioplatzer@gmail.com <emilioplatzer@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-09-02 15:49:53 +0000

    Agregamos controles al Armador de Html para que verifique la existencia de tags, los atributos dentro de los tags y el uso de abrir y cerrar o solo abrir de los distintos tipos de elementos (por ejemplo input solo abre).

--- trunk/fuentes/prueba1/armador-html.php
+++ trunk/fuentes/prueba1/armador-html.php
@@ -1,23 +1,54 @@
 <?php
 
-function enviar_abrir($tag, $atributos=array()){
-    echo "<$tag";
-    foreach($atributos as $nombre_atributo=>$contenido){
-        echo " $nombre_atributo=".'"'.str_replace(array('"','<', '>', '&'), array('\\"', '&lt;', '&gt;', '&amp;'), $contenido).'"';
+class ArmadorHtml{
+    private $attr_globales=array(
+        'id'=>array(),
+        'class'=>array()
+    );
+    private $tags=array(
+        'input'=>array('attr'=>array('name'=>array(), 'type'=>array()), 'solo_se_abre'=>true),
+        'label'=>array('attr'=>array('for'=>array())),
+        'table'=>array('attr'=>array()),
+        'td'   =>array('attr'=>array('title'=>array())),
+        'tr'   =>array('attr'=>array()),
+    );
+    private function controlar_tag($tag,$solo_se_abre){
+        if(!isset($this->tags[$tag])){
+            throw new Exception("ArmadorHtml no existe el tag '$tag'");
+        }
+        if(!isset($this->tags[$tag]['solo_se_abre']) && $solo_se_abre){
+            throw new Exception("ArmadorHtml el tag '$tag' debe cerrarse y abrirse");
+        }
+        if(isset($this->tags[$tag]['solo_se_abre']) && !$solo_se_abre){
+            throw new Exception("ArmadorHtml el tag '$tag' es de solo abrir");
+        }
+        return array_merge($this->attr_globales, $this->tags[$tag]['attr']);
     }
-    echo ">";
-}
-
-function enviar_texto($texto){
-    echo str_replace(array('<', '>', '&'), array('&lt;', '&gt;', '&amp;'), $texto);
-}
-
-function enviar_cerrar($tag){
-    echo "</$tag>";
-}
-
-function enviar_elemento($tag, $atributos=array()){
-    enviar_abrir($tag, $atributos);
+    private function tag_con_atributos($tag, $atributos, $solo_se_abre){
+        $definicion_tag=$this->controlar_tag($tag, $solo_se_abre);
+        echo "<$tag";
+        foreach($atributos as $nombre_atributo=>$contenido){
+            if(!isset($definicion_tag[$nombre_atributo])){
+                echo ">"; // para cerrar el tag y que se vea la excepcion
+                throw new Exception("ArmadorHtml no existe el atributo '$nombre_atributo' en '$tag'");
+            }
+            echo " $nombre_atributo=".'"'.str_replace(array('"','<', '>', '&'), array('\\"', '&lt;', '&gt;', '&amp;'), $contenido).'"';
+        }
+        echo ">";
+    }
+    function abrir($tag, $atributos=array()){
+        return $this->tag_con_atributos($tag, $atributos, false);
+    }
+    function texto($texto){
+        echo str_replace(array('<', '>', '&'), array('&lt;', '&gt;', '&amp;'), $texto);
+    }
+    function cerrar($tag){
+        $this->controlar_tag($tag, false);
+        echo "</$tag>";
+    }
+    function elemento($tag, $atributos=array()){
+        return $this->tag_con_atributos($tag, $atributos, true);
+    }
 }
 
 ?>
