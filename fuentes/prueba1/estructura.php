b72ec83f5ad85770e539cf37d4ef3d1a7528f287
emilioplatzer@gmail.com <emilioplatzer@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-09-04 14:45:24 +0000

    Enviamos HTML a través de meter metavariables

--- fuentes/prueba1/estructura.php
+++ fuentes/prueba1/estructura.php
@@ -2,13 +2,13 @@
 //UTF-8:SÍ
 
 $estructura_personal=array(
-    'per_documento'=>array('leyenda'=>'DNI'      , 'aclaracion'=>'sin puntitos'),
-    'per_cuil'     =>array('leyenda'=>'CUIL'     , 'aclaracion'=>'sin guiones' ),
-    'per_nombre'   =>array('leyenda'=>'Nombre'   , ),
-    'per_apellido' =>array('leyenda'=>'Apellido' , ),
-    'per_telefono' =>array('leyenda'=>'Teléfono' , ),
-    'per_mail'     =>array('leyenda'=>'Mail'     , ),
-    'per_direccion'=>array('leyenda'=>'Dirección', ),
+    'per_documento'=>array('leyenda'=>'DNI'            , 'aclaracion'=>'sin "puntitos" ni otros signos'),
+    'per_cuil'     =>array('leyenda'=>'CUIL'           , 'aclaracion'=>'sin guiones' ),
+    'per_nombre'   =>array('leyenda'=>'Nombre'         , ),
+    'per_apellido' =>array('leyenda'=>'Apellido'       , ),
+    'per_telefono' =>array('leyenda'=>'Teléfono'       , ),
+    'per_mail'     =>array('leyenda'=>'Mail "personal"', ),
+    'per_direccion'=>array('leyenda'=>'Dirección'      , ),
 );
 
 adaptar_estructura_campos($estructura_personal);
--- fuentes/prueba1/personal.php
+++ fuentes/prueba1/personal.php
@@ -10,32 +10,10 @@ function armaFormulario($titulo, $array_campos){
 HTML;
     $enviar=new ArmadorHtml();  
     foreach($array_campos as $campo=>$definicion_campo){
-        $leyenda=$definicion_campo['leyenda'];
-        $aclaracion=$definicion_campo['aclaracion'];
-        $enviar->abrir('tr');
-          $enviar->abrir('td',array('class'=>'etiqueta_formulario', 'title'=>$aclaracion));
-            $enviar->abrir('label',array('for'=>$campo));
-              $enviar->texto($leyenda);
-            $enviar->cerrar('label');
-          $enviar->cerrar('td');
-          $enviar->abrir('td');
-            $enviar->elemento('input',array('id'=>$campo, 'name'=>$campo, 'type'=>'text'));
-          $enviar->cerrar('td');
-        $enviar->cerrar('tr');
-        
-        /*
-        seguro($leyenda,'html');
-        seguro($aclaracion,'attr_html');
-        $str_html=$str_html."
-            <tr>
-              <td class=etiqueta_formulario title='$aclaracion'>$leyenda</td>     
-              <td><input id=$campo name=$campo type='text'></td> 
-            </tr>";
-        */
         $enviar->complejo(<<<HTML
             <tr>
-              <td class=etiqueta_formulario title=#:aclaracion>#!leyenda</td>
-              <td><input id=#:campo name=#:campo type='text'></td> 
+              <td class=etiqueta_formulario title="#aclaracion">#leyenda</td>
+              <td><input id="#nombre_campo" name="#nombre_campo" type='text'></td> 
             </tr>
 HTML
             , $definicion_campo
