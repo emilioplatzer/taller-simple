7489ab54b7d1c7d6a2176f3abdc9dea518aec122
emilioplatzer@gmail.com <emilioplatzer@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-09-09 15:56:10 +0000

    Listado con columnas filtradas.

--- trunk/fuentes/prueba1/estructura.php
+++ trunk/fuentes/prueba1/estructura.php
@@ -3,12 +3,12 @@
 
 $estructura_personal=array(
     'per_documento'=>array('leyenda'=>'DNI'            , 'aclaracion'=>'sin "puntitos" ni otros signos'),
-    'per_cuil'     =>array('leyenda'=>'CUIL'           , 'aclaracion'=>'sin guiones' ),
-    'per_nombre'   =>array('leyenda'=>'Nombre'         , ),
-    'per_apellido' =>array('leyenda'=>'Apellido'       , ),
-    'per_telefono' =>array('leyenda'=>'Teléfono'       , ),
-    'per_mail'     =>array('leyenda'=>'Mail "personal"', ),
-    'per_direccion'=>array('leyenda'=>'Dirección'      , ),
+    'per_cuil'     =>array('leyenda'=>'CUIL'           ,'listados'=>array('general'=>false), 'aclaracion'=>'sin guiones'),
+    'per_nombre'   =>array('leyenda'=>'Nombre'         ,'listados'=>array('agenda'=>true) ),
+    'per_apellido' =>array('leyenda'=>'Apellido'       ,'listados'=>array('agenda'=>true) ),
+    'per_telefono' =>array('leyenda'=>'Teléfono'       ,'listados'=>array('agenda'=>true) ),
+    'per_mail'     =>array('leyenda'=>'Mail "personal"','listados'=>array('agenda'=>true) ),
+    'per_direccion'=>array('leyenda'=>'Dirección'      ),
 );
 
 adaptar_estructura_campos($estructura_personal);
@@ -17,7 +17,12 @@ function adaptar_estructura_campos(&$estructura_tabla){
     foreach($estructura_tabla as $campo=>&$definicion_campo){
         $definicion_campo+=array(
             'aclaracion'=>'',
-            'nombre_campo'=>$campo
+            'nombre_campo'=>$campo,
+            'listados'=>array()
+        );
+        $definicion_campo['listados']+=array(
+            'agenda'=>false,
+            'general'=>true
         );
     }
 }
