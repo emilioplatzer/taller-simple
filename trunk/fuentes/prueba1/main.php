07c37cf5e7942a8d0935faaeb0e49f90277db229
raquelzssalva@gmail.com <raquelzssalva@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-09-09 12:56:23 +0000

    intento de listado simple. No funciona todavia

--- trunk/fuentes/prueba1/main.php
+++ trunk/fuentes/prueba1/main.php
@@ -7,6 +7,7 @@ require_once("estructura.php");
 require_once("despachador.php");
 require_once("menu.php");
 require_once("agregar-personal.php");
+require_once("listar-personal.php");
 
 despachar_parte_del_programa_segun_url();
 
