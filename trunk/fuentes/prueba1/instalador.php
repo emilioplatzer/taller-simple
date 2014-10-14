6f47b572280e77d5b59c8bcac80cfe9d04a3c395
emilioplatzer@gmail.com <emilioplatzer@gmail.com@de9674db-86d3-0d81-1670-04075e1139c5>
2014-10-14 19:19:28 +0000

    Hacemos que el instalador instale todas las tablas.

--- trunk/fuentes/prueba1/instalador.php
+++ trunk/fuentes/prueba1/instalador.php
@@ -1,18 +1,24 @@
 <?php
 
+require_once "comunes.php";
 require_once "estructura.php";
 require_once "db.class.php";
 
-$sql_campos=array();
-foreach($estructura_personal as $campo_id=>$campo_def){
-    $sql_campos[]="  $campo_id text";
+echo "entidades ".var_export($modelo['entidades'],true);
+echo "\n\n\n\n";
+
+foreach($modelo['entidades'] as $entidad_id=>$entidad_def){
+    $sql_campos=array();
+    foreach($entidad_def['campos'] as $campo_id=>$campo_def){
+        $sql_campos[]="  $campo_id text";
+    }
+
+    $sql="CREATE TABLE prin.".$entidad_def['nombre_tabla']." (\n".implode(",\n",$sql_campos)."\n);\n";
+
+    echo "por ejecutar:\n".$sql;
+
+    $db=abrir_conexion();
+    $db->exec($sql);
 }
-
-$sql="CREATE TABLE prin.personal (\n".implode(",\n",$sql_campos)."\n);\n";
-
-echo "por ejecutar:\n".$sql;
-
-$db=abrir_conexion();
-$db->exec($sql);
 
 ?>
